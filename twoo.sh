b9e416185573d994f0a8d572b343074e1449b75a
root <shashankgowtham12@gmail.com>
2022-01-31 13:10:43 +0000

    Edited the Twoo.sh file

--- twoo.sh
+++ twoo.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-((sum=35+35)
-echo "$sum"
+echo -e "Enter the file name: /c"
+read filename
+
+if [ -f $filename ]
+then
+   echo "file exist"
+else
+   echo "file doesn't exist"
+fi
